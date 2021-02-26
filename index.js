cf992918020e9182db45b39927318592fcc67250
Richard Lu <yubo.richard.lu@gmail.com>
2021-02-26 13:41:04 -0600

    phase1

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
     console.log('signed in')
+    let response = await fetch(`https://feed2json.org/convert?url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fwallstreetbets%2F.rss`)
+    let json = await response.json()
+    let feed = json.results
+    console.log(feed)
   } else {
     // Signed out
     console.log('signed out')
