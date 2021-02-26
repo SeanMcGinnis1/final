0204f895572e26c06ba02763ce178adf3a922a8c
Richard Lu <yubo.richard.lu@gmail.com>
2021-02-26 13:52:16 -0600

    json

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ firebase.auth().onAuthStateChanged(async function(user) {
     console.log('signed in')
     let response = await fetch(`https://feed2json.org/convert?url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fwallstreetbets%2F.rss`)
     let json = await response.json()
-    let feed = json.results
-    console.log(feed)
+    //let feed = json.results
+    console.log(json)
   } else {
     // Signed out
     console.log('signed out')
