c3b88ae27540bf75aad128aac53ed88097b0b9a2
Richard Lu <yubo.richard.lu@gmail.com>
2021-03-03 12:51:52 -0600

    push

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ firebase.auth().onAuthStateChanged(async function(user) {
     let response = await fetch(`https://feed2json.org/convert?url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fwallstreetbets%2F.rss`)
     let json = await response.json()
     //let feed = json.results
-    console.log(json)
+    
+    parsecontent(json)
   } else {
     // Signed out
     console.log('signed out')
@@ -25,3 +26,19 @@ firebase.auth().onAuthStateChanged(async function(user) {
     ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 })
+
+function parsecontent(json) {
+ 
+  var posts = []
+  for (let i = 0; i<json.items.length; i++) {
+    
+    let post = {
+      postTitle: `${json.items[i].title}`,
+      postDateStamp: `${json.items[i].date_published}`,
+      content_html: `${json.items[i].content_html}`
+    }
+    posts.push(post)
+  }
+  console.log(posts)
+}
+
