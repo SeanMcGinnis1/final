dd4b59f150130de9731d81350d5c93c59bcc13f9
Sean McGinnis <sean.mcginnis@kellogg.northwestern.edu>
2021-03-09 16:40:06 -0600

    another push

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ firebase.auth().onAuthStateChanged(async function(user) {
     generateGraph(db,user)
 
 
-    //await fetch('/.netlify/functions/feedProcessing') - turn off for UI testing
+    await fetch('/.netlify/functions/feedProcessing')
+    await fetch('/.netlify/functions/addskimmedtickers')
+
+    generateGraph(db,user)
      
   } else {
     // Signed out
